e9ad1d64fe8965aab16f0a6e4b7e246884931126
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2022-10-25 23:28:32 +0600

    Update welcome_redirect.blade.php

--- resources/views/home/welcome_redirect.blade.php
+++ resources/views/home/welcome_redirect.blade.php
@@ -6,6 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <meta name="description" content="This a ">
+	<meta name="keywords" content="HTML, CSS, JavaScript">
+	<meta name="author" content="jtks it">
+
     <meta property="og:title"              content="{{$title}}" />
     <meta property="og:image" 			   content="{{$image}}" />
     <meta property="og:description"        content="{{$title}}" />
