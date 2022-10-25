4f28ca2cf952980d069bebdc2b108813d55beeb7
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2022-10-25 18:06:56 +0600

    Update welcome_redirect.blade.php

--- resources/views/home/welcome_redirect.blade.php
+++ resources/views/home/welcome_redirect.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <meta property='og:title' content='Short link Generator with custom thumbnail and title'/>
-    <meta property='og:image' content='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
-    <meta property='og:description' content='Short link Generator with custom thumbnail and title 100% Free membership'/>
-    <meta property='og:url' content='https://jomaddarit.com/'/>
-    <meta name="twitter:image" content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+    <meta property="og:title"              content="{{$title}}" />
+    <meta property="og:image" 			   content="{{$image}}" />
+    <meta property="og:description"        content="{{$title}}" />
+    <meta property="og:url"                content="{{url('/')}}" />
+    <meta name="twitter:image" 			   content="{{$image}}" />
+       
     <meta property='og:image:width' content='1200' />
     <meta property='og:image:height' content='627' />
     <title>SHORT Link Generate</title>
