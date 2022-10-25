b09cae50baf783941d8613d0be9038dde078cc4b
Mr.Al-Amin Hossain <jomaddaralamin69@gmail.com>
2022-10-25 17:56:07 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,33 @@
 <head>
 
   <title>Free Short Link Generate with custom thumbnail and title </title>
-  <meta name="_token" content="{{ csrf_token() }}">
+  
   <!-- ** Mobile Specific Metas ** -->
   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
+    <meta property='og:title' content='Short link Generator with custom thumbnail and title'/>
+    <meta property='og:image' content='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+    <meta property='og:description' content='Short link Generator with custom thumbnail and title 100% Free membership'/>
+    <meta property='og:url' content='https://jomaddarit.com/'/>
+    <meta name="twitter:image" content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+    <meta property='og:image:width' content='1200' />
+    <meta property='og:image:height' content='627' />
+	
+  	<!-- <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta property="og:title"              content="{{$title}}" />
     <meta property="og:image" 			   content="{{$image}}" />
     <meta property="og:description"        content="{{$title}}" />
     <meta property="og:url"                content="{{url('/')}}" />
     <meta name="twitter:image" 			   content="{{$image}}" />
     <meta property='og:image:width' content='1200' />
-    <meta property='og:image:height' content='627' />
+    <meta property='og:image:height' content='627' /> -->
 
+
+	<meta name="_token" content="{{ csrf_token() }}">
 	
   <!-- favicon -->
   <link href="{{ URL::to('mages/favicon.png')}}" rel="shortcut icon">
